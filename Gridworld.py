d64c35a0478e4df917349aef2f89089223a60ad1
Lena Langholf <lenala39@gmail.com>
2018-03-01 23:11:07 +0100

    Thursday Eve: Fixed values creation

--- Gridworld.py
+++ Gridworld.py
@@ -1,4 +1,5 @@
 import manageIO;
+import calculate;
 
 class Gridworld:
     '''
@@ -13,8 +14,9 @@ class Gridworld:
         self.targetpolicy = []
         self.behaviorpolicy = []
         self.values = []
-        self.epsilon = 0
-        self.alpha = 0
+        self.converged = False
+        self.epsilon = 0.5
+        self.alpha = 0.5
         self.GAMMA = 1
         self.REWARD = -0.04
         self.PITFALL = -1
@@ -48,8 +50,78 @@ class Gridworld:
         return policy
 
 
+    def runEpisode(self):
+        '''
+        Simulates an episode from start (currently always the bottom right corner) to
+        finish (reaching a goal state) and updates the knowledge (in form of the q-values)
+        in the process. How the episode is run depends on the user's choice.
+        manual: shows the user each step and the updated q-value-array for each step
+        automatic: runs entire episode without output until end of episode is reached
+        (whether or not it then prints the q-value-function depends on whether the user
+        has chosen the single-episode mode or the full-automatic mode).
+        '''
+
+        # set the current state to its initial position (bottom left corner)
+        currentstate = (len(self.grid) - 1, 0)
+
+        iterations = 0
+        # run this until we reach a goalstate
+        while self.grid[currentstate[0]][currentstate[1]] != "E":
+            # determine action (epsilon-soft)
+            action = calculate.ActionSelection(self, currentstate)
+            # determine the next state given our current state and the chosen action
+            nextstate = calculate.nextState(self, currentstate, action)
+            # update the value function for this state-action pair
+            calculate.qUpdate(self, currentstate, action, nextstate)
+            # for manual processing: print the now updated value function
+            #TODO: this if-condition may increase runtime noticeably. Check this.
+            if self.processingMode == "m":
+                print("Updated q-values:")
+                manageIO.printValues(self)
+            # move from current state to next state
+            currentstate = nextstate
+            iterations = iterations + 1
+
+            print("Number of iterations: " + str(iterations))
+
+
 if __name__ == '__main__':
     test = Gridworld();
     #manageIO.readUserInput(test); Habe ein Grid als default gesetzt
     test.targetpolicy = test.initializePolicy(0.25)
     test.behaviorpolicy = test.initializePolicy(0)
+    # initialize action-value function with 0
+    test.values = [[[0 for actions in range(len(test.actions))] for column in range(len(test.grid[0]))] for row in range(len(test.grid))]
+
+    # if fully automatic processing mode is chosen
+    if test.processingMode == "a":
+        # run episodes until stopping criterion (convergence) is met
+        while not test.converged:
+            test.runEpisode()
+            # checks if policies converge and if they don't updates policy
+            calculate.updatePolicy(test)
+
+        print("The suggested q-values after convergence of policies are:")
+        manageIO.printValues(test.values)
+
+    # if semi-automatic or manual processing mode is chosen
+    else:
+        # run initial episode
+        test.runEpisode()
+        print("do we get here?")
+        # update policy accordingly
+        calculate.updatePolicy(test)
+
+        # ask the user after each episode if he or she wants to continue
+        while manageIO.continuationRequest() is True:
+            test.runEpisode()
+            calculate.updatePolicy(test)
+            # if convergence has occurred, notify user
+            # he or she may continue anyway if desired
+            if test.converged:
+                print("This episode did not change the recommended policies.")
+            else:
+                print("Policy has been updated.")
+
+        print("The suggested q-values after convergence of policies are:")
+        manageIO.printValues(test.values)
---
+++ calculate.py
@@ -0,0 +1,202 @@
+# TODO: Laura's code follows a standard that functions start capitalized, Lena's and mine does not. Decide on that.
+def ActionSelection(Gridworld, state):
+    '''
+    Selects the action with the highest q-value for the given state and updates the policy according to the epsilon-soft policy
+    :param state: a tuple with the coordinates of the current state
+    :return: the action to execute next
+    '''
+
+    max = Gridworld.values[state[0]][state[1]][0]
+    action = "up"
+
+    # find the maximal qvalue in the current state and the according action
+    for i in range(4):
+        inGrid = validNextState(Gridworld, state, Gridworld.actions[i])
+        if Gridworld.values[state[0]][state[1]][i] > max and inGrid:
+
+            # set new value for action that leads out of grid very low
+            # otherwise might be stuck in endless loop if highest action is outside
+            if not inGrid:
+                Gridworld.values[state[0]][state[1]][i] = -10
+
+            max = Gridworld.values[state[0]][state[1]][i]
+            action = Gridworld.actions[i]
+
+        # update the behaviorpolicy with the epsilon-soft formula
+    for i in Gridworld.actions:
+        # TODO: Still sometimes NoneType? What to do in that case?
+        if Gridworld.behaviorpolicy[state[0]][state[1]][i] is not None:
+            if i == action:
+                print("BehaviourPolicy: " + str(Gridworld.behaviorpolicy[state[0]][state[1]][i]))
+                Gridworld.behaviorpolicy[state[0]][state[1]][i] = 1 - Gridworld.epsilon + (
+                        Gridworld.epsilon / len(Gridworld.actions))
+                print("Updated BehaviorPolicy: " + str(Gridworld.behaviorpolicy[state[0]][state[1]][i]))
+            else:
+                Gridworld.behaviorpolicy[state[0]][state[1]][i] = Gridworld.epsilon / len(Gridworld.actions)
+    return action
+
+
+def DerivePolicy(Gridworld):
+    '''
+    make a policy which always chooses the action with the highest q-value
+    :return: greedy policy
+    '''
+
+    # initialize the policy
+    gredpol = [[None for y in range(len(Gridworld.values))] for x in range(len(Gridworld.values[0]))]
+
+    # go over the whole policy
+    for i in range(len(Gridworld.values)):
+        for j in range(len(Gridworld.values[0])):
+
+            max = Gridworld.values[i][j][1]
+            action = "up"
+
+            # find the maximal q-value in the state and the according action
+            for k in range(4):
+                if Gridworld.values[i][j][k] > max:
+                    max = Gridworld.values[i][j][k]
+                    action = Gridworld.actions[k]
+
+            # update the state of the policy
+            gredpol[i][j] = action
+
+    return gredpol
+
+
+def qUpdate(Gridworld, state, action, nextstate):
+    '''
+    Updates the q-value (action-value) for a given state and action.
+    :param: Gridworld: the Gridworld we operate in
+    :param: state: a tuple with the coordinates of the current state
+    :param: action: the action that is used to get to the next state
+    :param: nextstate: the state the state-action pair will result in
+    '''
+
+    # TODO: check with the others if variable name is okay
+    # TODO: also check with others if this somewhat convoluted solution is okay
+    # determine the immediate reward for performing a given action in the current state
+    # by seeing the immediate reward provided by the next state
+    r = immediateReward(Gridworld, nextstate)
+    # determine what the highest action-value of the next state is
+    nextQmax = max(Gridworld.values[nextstate[0]][nextstate[1]])
+    print("Highest action value: " + str(nextQmax))
+
+    # calculate action-value of the current action given the current state
+    # result of the immediate reward plus the best action-value of the next states
+    # remember: Gridworld.alpha is the learning rate of our learner
+    Gridworld.values[state[0]][state[1]][Gridworld.actions.index(action)] += Gridworld.alpha * (
+            r + Gridworld.GAMMA * nextQmax - Gridworld.values[state[0]][state[1]][Gridworld.actions.index(action)])
+
+
+def immediateReward(Gridworld, next):
+    '''
+    Returns the immediate reward for reaching a particular State.
+    :param: Gridworld: the Gridworld we operate in
+    :param: next: the state we will enter next
+    '''
+
+    if next != None:
+        print("Next State: " + str(next[0]) + " " + str(next[1]))
+    # retrieve the string representing the nature of the field we are about to enter
+    nature = Gridworld.grid[next[0]][next[1]]
+
+    # return according reward
+    if nature == "E":
+        return Gridworld.GOAL
+    elif nature == "P":
+        return Gridworld.PITFALL
+    elif nature == "O":
+        # TODO: check with the others if this solution is okay
+        return -1000
+    elif nature == "F":
+        return Gridworld.REWARD
+
+
+def nextState(Gridworld, state, action):
+    '''
+    Determines the coordinates of the next state.
+    :param: Gridworld: the overall world we act upon
+    :param: state: the state currently occupied
+    :param: action: the action occuring in that states
+    :return: the coordinates of the next state
+    '''
+
+    if (action == "up"):
+        # TODO: check if state[0] is actually vertical or horizontal line
+        # check if we can even move in that direction given our current position
+        if (state[0] != 0 and Gridworld.grid[state[0]-1][state[1]] != "O"):
+            state = (state[0] - 1, state[1])
+        # TODO: check with the others if this solution is alright
+    elif (action == "down"):
+        # check if we can even move in that direction given our current position
+        if (state[0] != len(Gridworld.grid) - 1 and Gridworld.grid[state[0]+1][state[1]] != "O"):
+            state = (state[0] + 1, state[1])
+    elif (action == "left"):
+        # check if we can even move in that direction given our current position
+        if (state[1] != 0 and Gridworld.grid[state[0]][state[1]-1] != "O"):
+            state = (state[0], state[1] - 1)
+    elif (action == "right"):
+        # check if we can even move in that direction given our current position
+        if (state[1] != len(Gridworld.grid[0]) - 1 and Gridworld.grid[state[0]][state[1]+1] != "O"):
+            state = (state[0], state[1] + 1)
+
+    return state
+
+
+def updatePolicy(Gridworld):
+    '''
+    Updates the greedy policy after an episode. Checks for convergence before updating.
+    :param Gridworld: the Gridworld we are in
+    '''
+
+    newpolicy = DerivePolicy(Gridworld)
+    Gridworld.converged = comparePolicies(Gridworld.targetpolicy, newpolicy)
+    if not Gridworld.converged:
+        Gridworld.targetpolicy = newpolicy
+
+
+def comparePolicies(policyOne, policyTwo):
+    '''
+    Checks if two policies are similar.
+    :param: policyOne: The first policy to take into consideration.
+    :param: policyTwo: The second policy to take into consideration.
+    :return: True if policies are the same, False if policies are different
+    '''
+
+    for i in range(len(policyOne)):
+        for j in range(len(policyOne[0])):
+            if policyOne[i][j] != policyTwo[i][j]:
+                return False
+    return True
+
+
+def validNextState(Gridworld, state, action):
+    '''
+    Returns a boolean that determines whether the next move leads out of the grid
+    :param Gridworld: Grid to operate on
+    :param state: current state
+    :param action: proposed action
+    :return: boolean if action leads out of grid
+    '''
+    if (action == "up"):
+        # TODO: check if state[0] is actually vertical or horizontal line
+        # Edit Lena: changed state[0] and [1]
+        # check if we can even move in that direction given our current position
+        if (state[0] != 0):
+            return True
+        # TODO: check with the others if this solution is alright
+    elif (action == "down"):
+        # check if we can even move in that direction given our current position
+        if (state[0] != len(Gridworld.grid) - 1):
+            return True
+    elif (action == "left"):
+        # check if we can even move in that direction given our current position
+        if (state[1] != 0):
+            return True
+    elif (action == "right"):
+        # check if we can even move in that direction given our current position
+        if (state[1] != len(Gridworld.grid[0]) - 1):
+            return True
+
+    return False
